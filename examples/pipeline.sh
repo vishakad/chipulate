1179a77a2a8551621de34c7af08ccd3b2a6088d9
vishakad <vishakadatta@gmail.com>
2019-01-04 14:04:20 +0530

    Give choice between SE/PE runs of bwa in pipeline

--- examples/pipeline.sh
+++ examples/pipeline.sh
@@ -3,9 +3,11 @@ prefixes=(fastq-test.chip_reads fastq-test.control_reads)
 for outprefix in ${prefixes[@]}; do
     echo "Doing $outprefix now."
     bwa index yeast-genome.fa
-    bwa aln yeast-genome.fa ${outprefix}.fastq > ${outprefix}.sai
-    bwa samse yeast-genome.fa ${outprefix}.sai $file > ${outprefix}.sam
-    #bwa mem yeast-genome.fa ${outprefix}_R1.fastq ${outprefix}_R2.fastq > ${outprefix}.sam
+    #Uncomment the following two lines for single-end test run.
+    #bwa aln yeast-genome.fa ${outprefix}.fastq > ${outprefix}.sai
+    #bwa samse yeast-genome.fa ${outprefix}.sai $file > ${outprefix}.sam
+    #Uncomment the following line for paired-end test run.
+    bwa mem yeast-genome.fa ${outprefix}_R1.fastq ${outprefix}_R2.fastq > ${outprefix}.sam
     samtools view -b ${outprefix}.sam > ${outprefix}.bam
     samtools sort ${outprefix}.bam > ${outprefix}.sorted.bam
     samtools index ${outprefix}.sorted.bam
